3b63a771709cfc782693bd59e3c6a328bbd7e572
Adithya-K47 <karthiad@sheridancollege.ca>
2025-02-17 13:59:37 -0500

    Imported a package

--- Assets/Gym week6 Planning/Bullet Movement.cs
+++ Assets/Gym week6 Planning/Bullet Movement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BulletMovement : MonoBehaviour
 {
-    float speed 10f;
+    //float speed 10f;
     // Start is called before the first frame update
     void Start()
     {
