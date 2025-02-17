e20d08c465f5b43cf8d4cb2633ab12591c954374
Adithya-K47 <karthiad@sheridancollege.ca>
2025-02-17 16:37:23 -0500

    Added a timer

--- Assets/Scripts/Balloon.cs
+++ Assets/Scripts/Balloon.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class Balloon : MonoBehaviour
 {
     public float balloonSpeed = 1f;
+    float lifetime = 3f;
+    float timer = 0f;
     
 
     // Start is called before the first frame update
@@ -21,6 +23,12 @@ public class Balloon : MonoBehaviour
         transform.Translate(Vector3.up * balloonSpeed * Time.deltaTime);
 
         if (transform.position.y > 7)
+        {
+            Destroy(gameObject);
+        }
+
+        timer += Time.deltaTime;
+        if (timer >= lifetime)
         {
             Destroy(gameObject);
         }
